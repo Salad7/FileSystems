2d76b7e5f7eb3db3b8addf5cce073fdbcaa00432
msalad <msalad@uncc.edu>
2016-04-13 09:43:23 +0000

    Basic i/o handling

--- file_creator.cpp
+++ file_creator.cpp
@@ -1,4 +1,26 @@
-<sys/types.h>
-<fcntl.h>
-<stdlib.h>
-<unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char file;
+    char* file_ptr;
+    int permissions = 666;
+    cout << "Enter a file name: " << endl;
+    cin >> file;
+    cout << "What permissions should the file have? FOR NOW ASSUMING 666" << endl;
+    
+    file_ptr = &file;
+    creat(file_ptr,permissions); //Runs into error, consider what mode means
+    
+    
+    
+    
+}
+
