0a19c4dce2c725426ed8d55c51ae8394df811d1b
msalad <msalad@uncc.edu>
2016-04-22 20:53:19 +0000

    fixed some input/output errors

--- file_creator.cpp
+++ file_creator.cpp
@@ -12,19 +12,20 @@ using namespace std;
 int main()
 {
     std::map<std::string, int> m; //Hashmap to store the permissions
+    std::map<std::string,int>::iterator mode;
     m["read"] = 0400;
-    m["read and write"] = 0600;
-    m["read and execute"] = 0500;
-    m["read and write and execute"] = 0700;
-    m["write and execute"] = 300;
-    m["execute"] = 100;
+    m["read-write"] = 0600;
+    m["read-execute"] = 0500;
+    m["read-write-execute"] = 0700;
+    m["write-execute"] = 0300;
+    m["execute"] = 0100;
     
     //char file;
     char* file_ptr;
     char* test;
     //int permissions = 0666
     //int mode = 0600;
-    int mode;
+    //int mode;
     string perm;
     int val;
     cout << "Enter a file name:";
@@ -33,16 +34,15 @@ int main()
     //file_ptr = file;
     cout << "What permissions would u like to give to the owner of this group?" << endl;
     cout << "read" << endl;
-    cout << "read and write" << endl;
-    cout << "read and execute" << endl;
-    cout << "read and write and execute" << endl;
-    cout << "write and execute" << endl;
+    cout << "read-write" << endl;
+    cout << "read-execute" << endl;
+    cout << "read-write-execute" << endl;
+    cout << "write-execute" << endl;
     cout << "execute" << endl;
     cin >> perm;
-    mode = m[perm];
+    mode = m.find("execute");
     file_ptr = test;
-    cout << mode;
-    val = creat(file_ptr,mode);
+    val = creat(file_ptr,m.find(perm)->second);
     
     
     
--- file_writer.cpp
+++ file_writer.cpp
@@ -10,25 +10,39 @@ using namespace std;
 
 int main()
 {
-   
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; 
+     mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; 
     int flags = O_WRONLY;
+    char* file_ptr;
+    char test;
     size_t count;
-    char* p ;//"This_is_a_test"; //create a pointer to the char we want to write to
-    char* wrt;
+    char* p; //create a pointer to the char we want to write to
+    char* wrte; //Create a pointer to the string we want to write in the file
     char buf[20];
+    char* emp;
     ssize_t wd; //the write method returns a ssize_t
     
+    //cout << "Enter a file name and what you want to write";
+    //cin >> test;
     
-    int fd = open("my_file_1.txt",flags,mode); //Open the file
+    file_ptr = "my_file_1.txt";//&test;
+    //strcpy(file_ptr,test.);
     
-    cout << "What do u want to write to the file";
-    cin >> wrt;
-    p = wrt;
+    int fd = open(file_ptr,flags,mode); //Open the file
     
-    strcpy(buf,p);
-    count = strlen(p);
+    cout << endl;
+    cout << "What do u want to write to the file" << endl;
+    cin >> wrte;
+    
+    p = wrte; //we cant directly save so we have to do this
+    
+    strcpy(buf,p); //Copy the information from p to the buf
+    
+    cout << *p;
+    count = strlen(p); //Set the count equal to the length of p
+    
     wd = write(fd,buf,count);
+    
     close(wd);
+  
 }
 
