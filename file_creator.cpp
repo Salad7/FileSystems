8c9a426d878ffd5a690a2c4422c287bcbd3a9f5d
msalad <msalad@uncc.edu>
2016-04-22 18:38:32 +0000

    Updated user input

--- file_creator.cpp
+++ file_creator.cpp
@@ -4,22 +4,37 @@
 #include <unistd.h>
 #include <string>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+
 int main()
 {
+    std::map<std::string, int> m; //Hashmap to store the permissions
+    m["read"] = 
+    m["read and write"] = 0600;
+    m["read and execute"] = 0666
+    m["read and write and execute"] = 
+    
+    m["write and execute"]
+    
+    m["execute"]
+    
     //char file;
-    char* file_ptr = "my_file_1.txt";
+    char* file_ptr;
+    char* test;
     //int permissions = 0666
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+    int mode = 0600;
     int val;
-    int close;
-   // cout << "Enter a file name: " << endl;
-    //cin >> "/filename" + file_ptr;
+    cout << "Enter a file name:";
+    cin >> test;
     //cout << "What permissions should the file have? FOR NOW ASSUMING mode" << endl;
     //file_ptr = file;
-    val = creat(file_ptr,mode); //Runs into error, consider what mode means
+    cout << "What permissions would u like to give to the owner of this group?"
+    
+    file_ptr = test;
+    val = creat(file_ptr,mode);
     
     
     
